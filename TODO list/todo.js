8639c21e01eabe3b1be54260629aa7c8b54e770f
ShamboKay <josh.watson.k@gmail.com>
2024-07-27 00:25:34 +0100

    I have added a task iterator and some small css to clean it up a little bit.

--- TODO list/todo.js
+++ TODO list/todo.js
@@ -1,22 +1,62 @@
 const addTaskInput = document.getElementById("addTask");
 const clearTask = document.getElementById("clear");
 const completeTask = document.getElementById("complete");
-const inputTaskText = document.getElementById("writeTask");
 let taskList = [];
+let taskNumber = [];
 
 
 
 /* Grabs the value entered by the input to then be added into the list array */ 
 function addingTask () {
-    const anyText = addTaskInput.value;
-    taskList.push(addTaskInput.value);
+    let anyText = addTaskInput.value
+
+    if (anyText && anyText !== addTaskInput.placeholder) {    
+       
+            taskList.push({text: anyText, completed: false});
+        
+        updateTaskList()
+        
+    } else if (anyText == '') {
+        return null
+    }
+    addTaskInput.value = '';
     console.log(taskList);
 }
 
-function clearOldTask () {
+
+
+function updateTaskList() {
+const list = document.getElementById('taskOrder')
+    list.innerHTML = '';  
+
+    taskList.forEach((taskList, index)=> {
+        const li = document.createElement('li')
+        li.textContent = `Task ${index + 1}:  ${taskList.text}`;
+        if (taskList.completed) {
+            li.classList.add('completed');
+        }
+
+        const completeButton = document.createElement('button');
+        completeButton.textContent = taskList.completed ? 'Undo' : 'Complete';
+        completeButton.onclick = () => completeOldTask(index);
+        
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = 'Delete'
+        deleteButton.onclick = () => clearOldTask(index);
+        
+        li.appendChild(completeButton);
+        li.appendChild(deleteButton);
+        list.appendChild(li);
+    });
 
 }
 
-function completeOldTask () {
+function clearOldTask (index) {
+taskList[index].completed = !taskList[index].completed;
+updateTaskList();
+}
 
+function completeOldTask (index) {
+taskList.splice(index, 1)
+updateTaskList();
 }
